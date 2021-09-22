d8c9d4ed991b6ce34c28527a620976dacd0c71a2
gjj52019 <garyjjenks@verizon.net>
2021-09-22 15:13:26 -0400

    can now pull the data from drinks.js through the server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const fruits = require('./models/drinks.js');
+const drinks = require('./models/drinks.js');
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) =>{
     res.send('Welcome to the Gitpub App!');
 
+})
+
+app.get('/drinks',(req,res) =>{
+    res.send(drinks);
 })
 
 app.listen(port, () => {
