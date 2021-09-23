fd17a2ecedff333975907c306c7da0c259c4c1a6
gjj52019 <garyjjenks@verizon.net>
2021-09-23 08:36:39 -0400

    Added food menu and made all menus more accessable through hyperlinks back to main and jump etween food and drinks

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const drinks = require('./models/drinks.js');
+const foods = require('./models/foods.js')
 const app = express();
 const port = 3000;
 
+//standard welcome
 app.get('/', (req,res) =>{
-    res.send('Welcome to the Gitpub App!');
+    const template = `    
+<html>
+    <head>
+    <style type="text/css">
+        body {
+          color: steelblue;
+          background-color: blanchedalmond;
+        }
+      </style>
+    </head>
+    <body>
+        <h1>Welcome to the Gitpub App!</h1>
+         <h2>Please look through our menus</h2>
+         <p><a href="/drinks">Drink Menu</a></p>
+         <p><a href="/foods">Food Menu</a></p>
+    </body>
+</html>
+`
+    res.send(template);
 
 })
 
-// app.get('/drinks',(req,res) =>{
-//     res.send(drinks);
-// })
-
+//====================================
+//drinks
 app.get('/drinks',(req,res) =>{
     res.render('drinks_index.ejs',{allDrinks:drinks});
 });
@@ -24,7 +42,24 @@ app.get('/drinks/:id',(req,res) =>{
     });
 
 });
+//========================================
+//foods
 
+app.get('/foods',(req,res) =>{
+    res.render('foods_index.ejs',{allFoods:foods});
+});
+
+app.get('/foods/:id',(req,res) =>{
+
+    let food = req.params.id
+    res.render('foods_show.ejs',{
+            food: foods[req.params.id]
+    });
+
+});
+
+//============================================
+//listening
 app.listen(port, () => {
     console.log('listening on port', port);
 });
